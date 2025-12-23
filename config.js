26d5ea795f5575c796f931254f9e3db7b1569b01
bedstv <31879823+bedstv@users.noreply.github.com>
2025-12-23 10:38:26 +0800

    Update EXEC_URL to remove unnecessary parameters

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // （建議：把 config.example.js 複製成 config.js 再改）
 
 window.TRIP_CONFIG = {
-  EXEC_URL: "https://script.google.com/macros/s/AKfycbylAunkHhYX1JEiVCkRcYTTWguThuc5guHC7mcMzpnRIgbT-B5xrXFejmRDiu0l7TnD/exec?action=meta&callback=cb",
+  EXEC_URL: "https://script.google.com/macros/s/AKfycbylAunkHhYX1JEiVCkRcYTTWguThuc5guHC7mcMzpnRIgbT-B5xrXFejmRDiu0l7TnD/exec",
   API_KEY: "Italy-Trip-Is-Good",
 };
